7763babf647d33de2d11efe231520560479d11e7
soheilshahi <soheiltoxic@gmail.com>
2016-11-08 22:45:39 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-
-echo "A";
+curl_setopt_array(
+    $ch, array( 
+    CURLOPT_URL => 'http://imdb.com/',
+    CURLOPT_RETURNTRANSFER => true
+));
+ 
+$output = curl_exec($ch);
+echo $output;
